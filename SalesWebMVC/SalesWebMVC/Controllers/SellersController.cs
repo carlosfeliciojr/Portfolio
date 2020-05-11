31326b058ce0572e2dab1acfb30fbc344ee9b7cb
Carlos Felicio <felicio.contato@live.com>
2020-05-11 17:57:04 -0300

    SellerService e FindAll síncrono

--- SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        // Criada dependencia para SellerService
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Retorna uma lista de Seller.
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMVC/SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService 
+    {
+        // Criada dependência do context.
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        // Retorna uma lista com todos os vendedores no banco de dados.
+        public List<Seller> FindAll()
+        {
+            // Acessa a fonte de dados e converte para uma lista.
+            return _context.Seller.ToList();
+        }
+    }
+}
