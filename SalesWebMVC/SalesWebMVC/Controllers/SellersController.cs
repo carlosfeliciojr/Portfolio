2d7398f9b07a1d4c46eec629eb4dbe2102a218dc
Carlos Felicio <felicio.contato@live.com>
2020-05-13 18:15:05 -0300

    Update seller and custom service exception

--- SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Services;
+using SalesWebMVC.Services.Exceptions;
 using System.Collections.Generic;
 
 namespace SalesWebMVC.Controllers
@@ -52,6 +54,40 @@ namespace SalesWebMVC.Controllers
             return View(obj);
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
         [HttpPost]
         // Notação para segurança.
         [ValidateAntiForgeryToken]
@@ -73,20 +109,29 @@ namespace SalesWebMVC.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Details(int? id)
+        [HttpPost]
+        // Notação para segurança.
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
         {
-            if (id == null)
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
             {
                 return NotFound();
             }
-
-            var obj = _sellerService.FindById(id.Value);
-            if (obj == null)
+            catch (DbConcurrencyException)
             {
-                return NotFound();
+                return BadRequest();
             }
-
-            return View(obj);
         }
     }
 }
