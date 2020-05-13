ec0af03d5655affaf9ef70a04832178154873c5f
Carlos Felicio <felicio.contato@live.com>
2020-05-13 20:32:13 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMVC/SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/SalesWebMVC/Services/SellerService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace SalesWebMVC.Services
 {
-    public class SellerService 
+    public class SellerService
     {
         // Criada dependência do context.
         private readonly SalesWebMVCContext _context;
@@ -39,9 +39,18 @@ namespace SalesWebMVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
+
+
         }
 
         public async Task UpdateAsync(Seller obj)
@@ -60,7 +69,7 @@ namespace SalesWebMVC.Services
             {
                 throw new DbConcurrencyException(e.Message);
             }
-            
+
         }
     }
 }
